722cf36b4afd8b777bc54fb2520b5ac32c107cc7
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-07-27 18:36:24 +0300

    update

--- index.js
+++ index.js
@@ -17,7 +17,63 @@ _data.create('marks', 'kazys', pazymiai, (err) => {
         console.log(err);
         return false;
     }
+    const _data = require('./lib/data');
 
+    // _data.read('users', 'petras', (err, data) => {
+    //     if (err || !data) {
+    //         console.log('Nepavyko perskaityti failo...');
+    //         return false;
+    //     }
+
+    //     const obj = JSON.parse(data);
+    //     console.log(obj);
+    // })
+
+
+    // const pazymiai = [10, 2, 8, 4, 6];
+    // _data.create('marks', 'kazys', pazymiai, (err) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return false;
+    //     }
+
+    //     console.log('Failas sekmingai sukurtas!');
+
+    //     _data.read('marks', 'kazys', (err, data) => {
+    //         if (err || !data) {
+    //             console.log('Nepavyko perskaityti Kazio failo...');
+    //             return false;
+    //         }
+
+    //         const obj = JSON.parse(data);
+    //         console.log(obj);
+    //     })
+    // });
+
+    const petras = {
+        name: 'Petras',
+        age: 99,
+        car: 'Audi',
+        color: 'red'
+    }
+    _data.update('users', 'petras', petras, (err) => {
+        if (err) {
+            console.log(err);
+            return false;
+        }
+
+        console.log('Petro duomenys atnaujinti sekmingai.');
+
+        _data.read('users', 'petras', (err, data) => {
+            if (err || !data) {
+                console.log('Nepavyko perskaityti Petro failo...');
+                return false;
+            }
+
+            const obj = JSON.parse(data);
+            console.log(obj);
+        })
+    });
     console.log('Failas sekmingai sukurtas!');
 
     _data.read('marks', 'kazys', (err, data) => {
